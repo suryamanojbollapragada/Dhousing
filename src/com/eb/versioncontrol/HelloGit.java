3457e7b14988c274f9d637a9b188554810e592e3
SuryaManoj <suryamanoj@live.com>
2017-04-29 16:15:03 +0530

    This is initial commit by admin _SuryaManoj_admin

---
+++ src/com/eb/versioncontrol/HelloGit.java
@@ -0,0 +1,9 @@
+package com.eb.versioncontrol;
+
+public class HelloGit {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Git this is for first commit _ Admin User");
+	}
+}
+
